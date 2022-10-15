e21d7c051c8d8f2e4eca342a96bcdf094acfe698
Jonas Kruckenberg <iterpre@protonmail.com>
2022-10-15 23:20:18 +0200

    wip

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,5 +1,8 @@
+use std::collections::VecDeque;
+
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use ghost_cell::GhostToken;
+use typed_arena::Arena;
 
 #[derive(Default)]
 struct Small(usize);
@@ -7,9 +10,9 @@ struct Small(usize);
 #[derive(Default)]
 struct Big([usize; 32]);
 
-fn push_back_crate_small(n: usize) {
+fn push_back_static_rc_small(n: usize) {
     GhostToken::new(|ref mut token| {
-        let mut list = linked_list::LinkedList::new();
+        let mut list = linked_list::static_rc::LinkedList::new();
 
         for _ in 0..n {
             list.push_back(Small::default(), token);
@@ -19,19 +22,28 @@ fn push_back_crate_small(n: usize) {
     });
 }
 
+fn push_back_slab_small(n: usize) {
+    GhostToken::new(|ref mut token| {
+        let arena = Arena::new();
+        let mut list = linked_list::arena::LinkedList::new(&arena);
+
+        for _ in 0..n {
+            list.push_back(Small::default(), token);
+        }
+    });
+}
+
 fn push_back_std_small(n: usize) {
     let mut list = std::collections::LinkedList::new();
 
     for _ in 0..n {
         list.push_back(Small::default());
     }
-
-    list.clear();
 }
 
-fn push_back_crate_big(n: usize) {
+fn push_back_static_rc_big(n: usize) {
     GhostToken::new(|ref mut token| {
-        let mut list = linked_list::LinkedList::new();
+        let mut list = linked_list::static_rc::LinkedList::new();
 
         for _ in 0..n {
             list.push_back(Big::default(), token);
@@ -41,42 +53,79 @@ fn push_back_crate_big(n: usize) {
     });
 }
 
+fn push_back_slab_big(n: usize) {
+    GhostToken::new(|ref mut token| {
+        let arena = Arena::with_capacity(n);
+        let mut list = linked_list::arena::LinkedList::new(&arena);
+
+        for _ in 0..n {
+            list.push_back(Big::default(), token);
+        }
+    });
+}
+
 fn push_back_std_big(n: usize) {
     let mut list = std::collections::LinkedList::new();
 
     for _ in 0..n {
         list.push_back(Big::default());
     }
+}
 
-    list.clear();
+fn push_back_vec_big(n: usize) {
+    let mut list = VecDeque::with_capacity(n);
+
+    for _ in 0..n {
+        list.push_back(Big::default());
+    }
 }
 
 fn bench_fibs(c: &mut Criterion) {
-    let mut group = c.benchmark_group("push_back_small");
-    for i in [20, 40, 60, 80].iter() {
-        group
-            .bench_with_input(BenchmarkId::new("crate", i), i, |b, i| {
-                b.iter(|| push_back_crate_small(*i))
-            })
-            .throughput(Throughput::Elements(*i as u64));
-        group
-            .bench_with_input(BenchmarkId::new("std", i), i, |b, i| {
-                b.iter(|| push_back_std_small(*i))
-            })
-            .throughput(Throughput::Elements(*i as u64));
-    }
-    group.finish();
+    // let mut group = c.benchmark_group("push_back_small");
+    // for i in [20, 40, 60, 80].iter() {
+    //     group
+    //         .bench_with_input(BenchmarkId::new("static-rc", i), i, |b, i| {
+    //             b.iter(|| push_back_static_rc_small(*i))
+    //         })
+    //         .throughput(Throughput::Elements(*i as u64));
+
+    //     group
+    //         .bench_with_input(BenchmarkId::new("arena", i), i, |b, i| {
+    //             b.iter(|| push_back_slab_small(*i))
+    //         })
+    //         .throughput(Throughput::Elements(*i as u64));
+
+    //     group
+    //         .bench_with_input(BenchmarkId::new("std", i), i, |b, i| {
+    //             b.iter(|| push_back_std_small(*i))
+    //         })
+    //         .throughput(Throughput::Elements(*i as u64));
+    // }
+    // group.finish();
 
     let mut group = c.benchmark_group("push_back_big");
-    for i in [20, 40, 60, 80].iter() {
+    for i in [100, 300, 500, 700].iter() {
         group
-            .bench_with_input(BenchmarkId::new("crate", i), i, |b, i| {
-                b.iter(|| push_back_crate_big(*i))
+            .bench_with_input(BenchmarkId::new("static-rc", i), i, |b, i| {
+                b.iter(|| push_back_static_rc_big(*i))
             })
             .throughput(Throughput::Elements(*i as u64));
+
+        group
+            .bench_with_input(BenchmarkId::new("arena", i), i, |b, i| {
+                b.iter(|| push_back_slab_big(*i))
+            })
+            .throughput(Throughput::Elements(*i as u64));
+
         group
             .bench_with_input(BenchmarkId::new("std", i), i, |b, i| {
                 b.iter(|| push_back_std_big(*i))
+            })
+            .throughput(Throughput::Elements(*i as u64));
+
+        group
+            .bench_with_input(BenchmarkId::new("vec", i), i, |b, i| {
+                b.iter(|| push_back_vec_big(*i))
             })
             .throughput(Throughput::Elements(*i as u64));
     }
